d7a0ed59a5aeecaa24025d413e5ae6967b5ddb39
Christopher Greene <archgoon@gmail.com>
2024-10-12 18:54:34 -0700

    Implemented add.
    Wow that was annoying. Haven't tested it yet.

--- bignum.asm
+++ bignum.asm
@@ -8,13 +8,49 @@
 
     ; Set up screen memory location and text
     ldx #0                 ; Start index at 0
-loop:
-    lda message,x         ; Load the message character by character
-    beq end                ; If the character is null (0), end loop
-    sta $0400,x           ; Store character in screen memory ($0400 = top-left corner of C64 screen)
-    inx                    ; Increment index
-    bne loop               ; Repeat until the end of message
+    rts
 
+; we're going to use 0x1000 for a,b,c
+loadbig_a:
+    ; TODO: use X to select page, Y to select offset in page
+    ; and then copy them to 0x1000, 0x1100. Addition routines
+loadbig_b:
+
+add:
+    clc     ; clear carry
+    ; read length
+    lda 0x1000
+    sbc 0x1100
+    bmi b_bigger
+a_bigger:
+        ldy 0x1000
+        jmp _done
+b_bigger:
+        ldy 0x1100
+_done:
+    sty 0x1200
+    ldx #$1 ; 1 because first byte is size
+add_loop:
+        ; X and Y are the offsets
+        lda 0x1000,X
+        bcc _carry ; Branch on Carry Clear
+carry:
+            clc
+            adc 1 ; may set overflow
+_carry:        
+        adc 0x1100,X ; may set overflow flag
+        sta 0x1200,X
+        dey
+        bne add_loop
+    bcc _no_carry
+    
+    ; carry
+    ldy 0x1200
+    iny
+    sty 0x1200
+_no_carry:
+    rts
+    
 end:
     rts                    ; Return from subroutine
 
