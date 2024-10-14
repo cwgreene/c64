3bc27ca26586cc51ceef85a73c59d9004d09409a
Christopher Greene <archgoon@gmail.com>
2024-10-13 20:10:25 -0700

    Update test, getting closer to a sane calling convention.

--- test/test_bignum.py
+++ test/test_bignum.py
@@ -8,7 +8,7 @@ def mknum(a):
         a //= 256
     return arr
 
-def test_add(a,b):
+def create_add():
     # load program
     f = open("bignum.prg", "rb").read()
     mmu = MMU([
@@ -18,30 +18,42 @@ def test_add(a,b):
         mmu.write(0x801+i, by)
     c = CPU(mmu, 0x818)
 
-    a = mknum(a)
-    b = mknum(b)
+    def call_add(a,b):
+        # set address
+        c.r.pc = 0x818
 
-    # pad them to same length for zip
-    a += [0]*max(len(b)-len(a),0)
-    b += [0]*max(len(a)-len(b),0)
+        a = mknum(a)
+        b = mknum(b)
 
-    mmu.write(0x1000, len(a))
-    mmu.write(0x1100, len(b))
-    for i,(a_digit,b_digit) in enumerate(zip(a,b)):
-        mmu.write(0x1000+i+1,a_digit)
-        mmu.write(0x1100+i+1,b_digit)
+        # pad them to same length for zip
+        a += [0]*max(len(b)-len(a),0)
+        b += [0]*max(len(a)-len(b),0)
 
-    # simulate
-    while c.r.pc != 0x84f:
-        op = mmu.read(c.r.pc)
-        #print(hex(c.r.pc), hex(op), c.ops[op].args[1].__name__, c.r.a, c.r.x, c.r.y, bin(c.r.p))
-        c.step()
-    acc = 0
-    length = mmu.read(0x1200)
-    #print("length",length)
-    for i in range(length):
-        #print("digit", mmu.read(0x1200+i+1))
-        acc += mmu.read(0x1200+i+1)*256**i
-    return acc
-print(test_add(3,4), 3+4)
-print(test_add(2741,141234), 2741+141234)
+        # Write a and b
+        mmu.write(0x1000, len(a))
+        mmu.write(0x1100, len(b))
+        for i,(a_digit,b_digit) in enumerate(zip(a,b)):
+            mmu.write(0x1000+i+1,a_digit)
+            mmu.write(0x1100+i+1,b_digit)
+
+        # simulate
+        while c.r.pc != 0x84f:
+            op = mmu.read(c.r.pc)
+            #print(hex(c.r.pc), hex(op), c.ops[op].args[1].__name__, c.r.a, c.r.x, c.r.y, bin(c.r.p))
+            c.step()
+
+        # read out
+        acc = 0
+        length = mmu.read(0x1200)
+
+        for i in range(length):
+            #print("digit", mmu.read(0x1200+i+1))
+            acc += mmu.read(0x1200+i+1)*256**i
+        return acc
+    return call_add
+
+def test_add():
+    call_add = create_add()
+    for i in range(256):
+        for j in range(256):
+            assert call_add(i,j) == i+j
